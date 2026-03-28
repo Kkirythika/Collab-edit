885ec049b46b6ae99563e810c7aabea72ba1154d
Kiruthika <your-email@example.com>
2026-03-28 10:12:59 +0530

    Initial commit

--- demo/demo/src/main/java/com/example/demo/config/WebSocketConfig.java
+++ demo/demo/src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -4,9 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.lang.NonNull;   
-
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer; 
 
 
 @Configuration
@@ -14,13 +12,16 @@ import org.springframework.lang.NonNull;
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-editor").setAllowedOriginPatterns("*").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-editor")
+                .setAllowedOrigins("http://localhost:3000","http://localhost:3002","http://localhost:5173")
+                .withSockJS();
     }
 
     @Override
-    public void configureMessageBroker(@NonNull MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/editor", "/topic");
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/editor");
         registry.setApplicationDestinationPrefixes("/app");
     }
 }
+
